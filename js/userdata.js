2d3c17e5a64499d3b0d4f089628075d682418429
silver <mj_ol@outlook.com>
2020-01-02 19:39:50 +0900

    UPDATE

--- js/userdata.js
+++ js/userdata.js
@@ -1,4 +1,4 @@
 userData={
   "root": {"name":"root","passwd":"123456", "pic":"./images/avatar/1.jpg"},
-  "jecosine": {"name":"jecosine","passwd":"123456", "pic":"./image/avatar/2.jpg"}
+  "jecosine": {"name":"jecosine","passwd":"123456", "pic":"./images/avatar/2.jpg"}
 }
